977e5e70f393c8795734fb9c3c4964c2fbfeb713
Vinay Dhull <dhullv650@gmail.com>
2024-06-18 23:53:26 +0530

    first commit

---
+++ link.js
@@ -0,0 +1,14 @@
+var typed=new Typed(
+  '#element',{
+       strings: [ 
+          "HTML",
+          " CSS",
+          "JavaScript",
+          "Java",
+          "Data Structures and Algorithm",],
+          typeSpeed: 80,
+          loop: true,
+  }
+     
+);
+ 
